f5af69476178c023d6c79d9ae834284cb5977900
Andreas <60272972+AndreasDevCode@users.noreply.github.com>
2026-09-18 12:45:40 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  * mellomlagres aldri her, slik at dere aldri ser gamle lister.
  */
 
-var VERSION = 'handleliste-v2';
+var VERSION = 'handleliste-v5';
 var SHELL_CACHE = VERSION + '-shell';   // appens egne filer
 var VENDOR_CACHE = VERSION + '-vendor'; // firebase-sdk + skrifter
 
